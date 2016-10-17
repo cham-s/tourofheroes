d978cde68cdce5c3d2c20d3b7b660e9c938b4be2
cham-s <cattouma@student.42.fr>
2016-10-17 14:41:24 +0200

    git status

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+import { Router } from '@angular/router';
 
 @Component({
     moduleId: module.id,
@@ -12,12 +13,18 @@ import { HeroService } from './hero.service';
 export class DashboardComponent implements OnInit {  
     heroes: Hero[] = [];
     
-    constructor(private heroService: HeroService) {  }
+    constructor(
+        private heroService: HeroService,
+        private router: Router
+    ) {}
 
     ngOnInit(): void {
         this.heroService.getHeroes()
         .then(heroes => this.heroes = heroes.slice(1, 5));
     }
 
-    gotoDetail(hero: Hero): void { /* to be implemented */ } 
+    gotoDetail(hero: Hero): void {
+        let link = ['/detail', hero.id];
+        this.router.navigate(link);
+     } 
 }
